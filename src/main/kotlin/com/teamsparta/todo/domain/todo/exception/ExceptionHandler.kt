f96586064952bac95b35e42e415b43cd95b12ff6
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-16 17:51:15 +0900

    ✨feat : 에러메세지 응답을 위한 ErrorResponse 생성 및 적용
    StatusCode뿐만 아니라
    에러 메세지 또한 dto로 변환 후 반환하기 위해
    data class ErrorResponse 생성 후 적용

--- src/main/kotlin/com/teamsparta/todo/domain/todo/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/todo/exception/ExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.teamsparta.todo.domain.todo.exception
 
+import com.teamsparta.todo.domain.todo.exception.dto.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(ModelNotFoundException::class)
-    fun handleModelNotFoundException(e:ModelNotFoundException): ResponseEntity<Unit> {
+    fun handleModelNotFoundException(e:ModelNotFoundException): ResponseEntity<ErrorResponse> {
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
-            .build()
+            .body(ErrorResponse(message = e.message))
     }
 }
