bab3a5744e36ac11fbcb5e64a46c424fdb317c4d
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-16 16:24:32 +0900

    🎉create: TodoServiceImpl 비즈니스 로직 임시작성(주석 TODO)

--- src/main/kotlin/com/teamsparta/todo/domain/todo/service/TodoServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/todo/service/TodoServiceImpl.kt
@@ -8,22 +8,27 @@ import org.springframework.stereotype.Service
 @Service
 class TodoServiceImpl:TodoService {
     override fun getAllTodoList(): List<TodoResponse> {
+//      TODO : DB에서 모든 Todo 목록 조회하여 TodoResponse 목록으로 변환 후 반환
         TODO("Not yet implemented")
     }
 
     override fun getTodoById(todoId: Long): TodoResponse {
+//      TODO : DB에서 todoId에 해당하는 Todo를 가져와서 TodoReponse로 변환후 반환
         TODO("Not yet implemented")
     }
 
     override fun createTodo(request: CreateTodoRequest): TodoResponse {
+//      TODO : request를 Todo(Entity)로 변환 후 DB에 저장
         TODO("Not yet implemented")
     }
 
     override fun updateTodo(todoId: Long, request: UpdateTodoRequest): TodoResponse {
+//      TODO : DB에서 todoId에 해당하는 Todo를 가져와서 request 정보로 수정(Update) 후 DB에 저장, 결과를 TodoResponse로 변환 후 반환
         TODO("Not yet implemented")
     }
 
     override fun deleteTodo(todoId: Long) {
+//      TODO : DB에서 todoId에 해당하는 Tood를 삭제
         TODO("Not yet implemented")
     }
 }
