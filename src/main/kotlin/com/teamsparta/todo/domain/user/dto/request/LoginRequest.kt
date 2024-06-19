56516ab8f343a9d9f1e997653eccdf620a79bbc3
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-06-19 23:49:35 +0900

    ✨ feat : 회원가입, 로그인 시 유효성 검사 추가

--- src/main/kotlin/com/teamsparta/todo/domain/user/dto/request/LoginRequest.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/user/dto/request/LoginRequest.kt
@@ -1,7 +1,19 @@
 package com.teamsparta.todo.domain.user.dto.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
 data class LoginRequest(
+    @field:NotBlank(message = "Email address must not be blank")
+    @field:Email(message = "Email address should be format-valid")
     val email: String,
+
+    @field:NotBlank(message = "Password should not be blank")
+    @field:Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,15}$",
+        message = "Password should be at least 8 letters long and at most 15 characters long, including at least ONE uppercase, lowercase, and special letters."
+    )
     val password: String
 )
 
--- src/main/kotlin/com/teamsparta/todo/domain/user/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/teamsparta/todo/domain/user/dto/request/SignUpRequest.kt
@@ -1,8 +1,30 @@
 package com.teamsparta.todo.domain.user.dto.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+
 data class SignUpRequest(
+    @field:NotBlank(message = "Email address must not be blank")
+    @field:Email(message = "Email address should be format-valid")
     val email:String,
+
+    @field:NotBlank(message = "Password should not be blank")
+    @field:Pattern(
+        regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@\$!%*?&])[A-Za-z\\d@\$!%*?&]{8,15}$",
+        message = "Password should be at least 8 letters long and at most 15 characters long, including at least ONE uppercase, lowercase, and special letters."
+    )
     val password:String,
+
+    @field:NotBlank(message = "Username must not be blank")
+    @field:Pattern(
+        regexp = "^[a-z0-9]{4,10}$",
+        message = "Username should be [4, 10] letters long, only with lowercase letters and numbers."
+    )
     val nickname:String,
+
+
+    @field:NotBlank(message = "Password should not be blank")
     val role:String
 )
