c384063e77907bedc3ec7ad2e121ef7013428de6
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-07-01 11:28:07 +0900

    ✨ feat: test관련 dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ plugins {
 }
 
 val queryDslVersion = "5.0.0"
+val kotestVersion = "5.8.1"
+val mockkVersion = "1.13.8"
 
 group = "com.teamsparta"
 version = "0.0.1-SNAPSHOT"
@@ -45,7 +47,16 @@ dependencies {
 
 
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+	testImplementation("io.mockk:mockk:$mockkVersion")
+	testImplementation("com.ninja-squad:springmockk:4.0.2")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
